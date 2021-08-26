d621d7e8d7b9ac72ff43db64c28ab4e23ae3e2c1
ayangupta9 <ayangupta1000@gmail.com>
2021-08-27 00:16:23 +0530

    next js: Link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home () {
-  return <h1>Home Page</h1>
+  return (
+    <div>
+      <h1>Home Page</h1>
+
+      <ul>
+        <li>
+          <Link href='/blog'>
+            <a>To the Blogs</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/product'>
+            <a>To the Products</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,10 +1,33 @@
-export default function ProductList () {
+import Link from 'next/link'
+
+export default function ProductList ({ productid = 100 }) {
   return (
     <>
+      <Link href='/'>
+        <a>To Home</a>
+      </Link>
+
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <Link href='/product/1'>
+            <a>Product 1</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/product/2'>
+            <a>Product 2</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/product/3' replace>
+            <a>Product 3</a>
+          </Link>
+        </li>
+        <li>
+          <Link href={`/product/${productid}`}>
+            <a>Product {productid}</a>
+          </Link>
+        </li>
       </ul>
     </>
   )
