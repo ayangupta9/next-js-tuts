3d8876e8e286e86d99ce0042e90c7ff48af7cd37
ayangupta9 <ayangupta1000@gmail.com>
2021-08-26 23:43:50 +0530

    next js: dynamic nested routing

---
+++ pages/product/[productid]/review/[reviewid].js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/dist/client/router'
+
+function Review () {
+  const router = useRouter()
+  const { productid, reviewid } = router.query
+
+  return (
+    <h1>
+      Review {reviewid} for product {productid}
+    </h1>
+  )
+}
+
+export default Review
