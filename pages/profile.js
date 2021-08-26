c43814d5783440298b839196d898f2b577945f87
ayangupta9 <ayangupta1000@gmail.com>
2021-08-26 20:41:42 +0530

    Next JS - File names are considered routes

---
+++ pages/profile.js
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Profile () {
+  return <h1>Profile Page</h1>
+}
